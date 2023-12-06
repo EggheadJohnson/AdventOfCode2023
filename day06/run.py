3f86505b6d80801d2c86fb4d0bab5b232b684f7d
Paul Johnson <pejohnson@etsy.com>
2023-12-06 12:17:23 -0700

    refactor day 6 pt 2 and add the ability to time things

--- day06/run.py
+++ day06/run.py
@@ -1,9 +1,11 @@
 import os, argparse, config, logging
 from pprint import pformat
+from datetime import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--puzzle', action='store_true', help='Use the live puzzle data')
 parser.add_argument('--debug', action='store_true', help='Print debug statements')
+parser.add_argument('--time', action='store_true', help='Include execution time')
 args = parser.parse_args()
 
 if args.debug:
@@ -26,6 +28,21 @@ print('Opening {}'.format(file_name))
 
 inpt = [ line.strip() for line in open(input_path, 'r')]
 
-from solutions import part1, part2
-print("result Part 1:", part1(inpt))
-print("result Part 2:", part2(inpt))
+from solutions import part1, part2, part2b
+if args.time:
+    pt1Start = datetime.now()
+    pt1Result = part1(inpt)
+    pt1End = datetime.now()
+    print("result Part 1: {} time: {}.{}".format(pt1Result, (pt1End - pt1Start).seconds, (pt1End - pt1Start).microseconds))
+    pt2Start = datetime.now()
+    pt2Result = part2(inpt)
+    pt2End = datetime.now()
+    print("result Part 2: {} time: {}.{}".format(pt2Result, (pt2End - pt2Start).seconds, (pt2End - pt2Start).microseconds))
+    pt3Start = datetime.now()
+    pt3Result = part2b(inpt)
+    pt3End = datetime.now()
+    print("result Part 2b: {} time: {}.{}".format(pt3Result, (pt3End - pt3Start).seconds, (pt3End - pt3Start).microseconds))
+else:
+    print("result Part 1:", part1(inpt))
+    print("result Part 2:", part2(inpt))
+    print("result Part 2b:", part2b(inpt))
--- day06/solutions.py
+++ day06/solutions.py
@@ -1,4 +1,5 @@
 import pprint, sys, config, re
+from math import sqrt, ceil, floor
 sys.path.append('../python_utils')
 
 from Board2D import Board2D
@@ -10,8 +11,9 @@ debug = config.debug
 #############
 #
 # Results:
-# result Part 1: 345015
-# result Part 2: 42588603
+# result Part 1: 345015 time: 0.225
+# result Part 2: 42588603 time: 0.498503
+# result Part 2b: 42588603 time: 0.140
 #
 #############
 
@@ -41,3 +43,13 @@ def part2(input):
     debug(i, time, 1+time-(2*i))
     
     return 1+time-(2*i)
+
+def part2b(input):
+    timeDists = parseInput(input)
+    time = int(''.join([str(i[0]) for i in timeDists]))
+    dist = int(''.join([str(i[1]) for i in timeDists]))
+
+    debug((-1*time + sqrt(time**2 - 4*(-1)*(-1*dist))) / (2*-1))
+    debug((-1*time - sqrt(time**2 - 4*(-1)*(-1*dist))) / (2*-1))
+    boundaries = ((-1*time + sqrt(time**2 - 4*(-1)*(-1*dist))) / (2*-1), (-1*time - sqrt(time**2 - 4*(-1)*(-1*dist))) / (2*-1))
+    return 1+ floor(max(boundaries)) - ceil(min(boundaries))
